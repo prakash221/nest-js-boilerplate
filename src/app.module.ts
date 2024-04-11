1beb6954706ed0547c9375432b3845388c33ade3
Prakash Sharma <prakash.psc221@gmail.com>
2024-04-11 22:51:45 +0545

    added auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_DB_CONNECTION_STRING),
     UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
